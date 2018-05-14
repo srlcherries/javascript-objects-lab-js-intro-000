e86797b41f12241a2d92ce976647f0cdf8075535
Sherry Lin <sherrylin222@gmail.com>
2018-05-14 01:21:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var receipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
