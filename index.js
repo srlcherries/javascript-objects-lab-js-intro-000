8a178b6f4e2f88fc8f037d7855c727a32afa7194
Sherry Lin <sherrylin222@gmail.com>
2018-05-14 01:22:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var receipes = {};
+var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  return Object.assign({}, object, { [key]: value });
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+    object[key] = value;
+    return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
